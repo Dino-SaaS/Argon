12ce234343a80e2498bc2c62871dbb4d478f1cff
Tim <tjones4413@gmail.com>
2020-08-18 14:22:57 -0400

    Nucleo icons now load properly

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module ArgonV1
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
