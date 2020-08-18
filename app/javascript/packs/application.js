ede35190f3ff7c1e38e3fd4d05315b08d20a2e26
Tim <tjones4413@gmail.com>
2020-08-18 14:42:30 -0400

    Install JQuery and Popper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require('popper.js')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
