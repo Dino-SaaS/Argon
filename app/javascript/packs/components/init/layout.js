7cd4becbca0def677f02d7bf3f591cbc93372e09
Tim <tjones4413@gmail.com>
2020-08-18 18:47:10 -0400

    Add packs/components, fix duplicate <head> tag issue, get sidebar toggling working

---
+++ app/javascript/packs/components/init/layout.js
@@ -0,0 +1,98 @@
+/* Initializiation */
+
+$( document ).on('turbolinks:load', function() {
+  Layout();
+})
+'use strict';
+
+var Layout = function() {
+
+    function pinSidenav() {
+        $('.sidenav-toggler').addClass('active');
+        $('.sidenav-toggler').data('action', 'sidenav-unpin');
+        $('body').removeClass('g-sidenav-hidden').addClass('g-sidenav-show g-sidenav-pinned');
+        $('body').append('<div class="backdrop d-xl-none" data-action="sidenav-unpin" data-target='+$('#sidenav-main').data('target')+' />');
+    }
+
+    function unpinSidenav() {
+        $('.sidenav-toggler').removeClass('active');
+        $('.sidenav-toggler').data('action', 'sidenav-pin');
+        $('body').removeClass('g-sidenav-pinned').addClass('g-sidenav-hidden');
+        $('body').find('.backdrop').remove();
+    }
+
+    function initSideNav() {
+      if($(window).width() >= 1200) {
+          pinSidenav();
+      }
+
+      if($(window).width() < 1200){
+        unpinSidenav();
+      }
+    }
+
+    $(window).on('load resize', function() {
+      initSideNav();
+    });
+
+    $("body").on("click", "[data-action]", function(e) {
+
+        e.preventDefault();
+
+        var $this = $(this);
+        var action = $this.data('action');
+        var target = $this.data('target');
+
+
+        // Manage actions
+
+        switch (action) {
+            case 'sidenav-pin':
+                pinSidenav();
+            break;
+
+            case 'sidenav-unpin':
+                unpinSidenav();
+            break;
+
+            case 'search-show':
+                target = $this.data('target');
+                $('body').removeClass('g-navbar-search-show').addClass('g-navbar-search-showing');
+
+                setTimeout(function() {
+                    $('body').removeClass('g-navbar-search-showing').addClass('g-navbar-search-show');
+                }, 150);
+
+                setTimeout(function() {
+                    $('body').addClass('g-navbar-search-shown');
+                }, 300)
+            break;
+
+            case 'search-close':
+                target = $this.data('target');
+                $('body').removeClass('g-navbar-search-shown');
+
+                setTimeout(function() {
+                    $('body').removeClass('g-navbar-search-show').addClass('g-navbar-search-hiding');
+                }, 150);
+
+                setTimeout(function() {
+                    $('body').removeClass('g-navbar-search-hiding').addClass('g-navbar-search-hidden');
+                }, 300);
+
+                setTimeout(function() {
+                    $('body').removeClass('g-navbar-search-hidden');
+                }, 500);
+            break;
+        }
+    })
+
+    // Make the body full screen size if it has not enough content inside
+    $(window).on('load resize', function() {
+        if($('body').height() < 800) {
+            $('body').css('min-height', '100vh');
+            $('#footer-main').addClass('footer-auto-bottom')
+        }
+    })
+
+};
