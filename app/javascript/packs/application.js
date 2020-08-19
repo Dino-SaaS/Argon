ec490e558805b42cf20043278556211c93e915a2
Tim <tjones4413@gmail.com>
2020-08-19 16:16:16 -0400

    Fix bootstrap installation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
-require('popper.js')
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
