2add0dc7d8dc45d620aef24806e06c8cc8490b2b
Tim <tjones4413@gmail.com>
2020-08-26 15:03:30 -0400

    Add Chart.extension.js for rounded corners

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("chart.js")
+require("Chart.extension.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
