ae3527050786d2fc7975fcbaa4c5764acda1d2a5
Tim <tjones4413@gmail.com>
2020-08-26 15:17:51 -0400

    Get scrollbar library loading -- move argon.js out of javascript/packs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,12 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
 require("chart.js")
 require("Chart.extension.js")
+require("jquery-scrollLock.min.js")
+require("jquery.scrollbar.min.js")
+require("argon.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
