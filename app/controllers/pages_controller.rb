49472db5f90b806d5b81b99c6215b6c6e2cfb62d
Tim <tjones4413@gmail.com>
2020-09-15 15:38:46 -0400

    Pull sidenav and topnav into dashboard layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,14 @@
 class PagesController < ApplicationController
+  layout :resolve_layout
+
+  private
+
+  def resolve_layout
+    case action_name
+    when ['login', 'register']
+      'authentication'
+    else
+      'dashboard'
+    end
+  end
 end
